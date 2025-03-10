16092d6d502ad1b54ac62fb302a21857e89c7cc0
Oscarfdo <oscarfdo25@gmail.com>
2025-03-10 10:56:38 -0700

    Creación del archivo Implicito

--- KWIC Implicit Invocation/Eventos/Main.java
+++ KWIC Implicit Invocation/Eventos/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         MasterControl masterControl = new MasterControl();
 
-        Input inputReader = new Input(masterControl, "KWIC Implicit Invocation\\Eventos\\Leer.txt");
+        Input inputReader = new Input(masterControl, "/Users/kueven/Documents/Proyectos-Conjuntos/Proyectos-Conjuntos/KWIC Implicit Invocation/Eventos/Leer.txt");
         CircularShift shifter = new CircularShift(masterControl);
         Alphabetizer alphabetizer = new Alphabetizer(masterControl);
         Output outputWriter = new Output(masterControl);
